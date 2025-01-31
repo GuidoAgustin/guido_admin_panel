2257adf2da57236b18a1a4368e1aeb34fccc5b3b
Federico Arena <fedarena@gmail.com>
2025-01-31 20:00:22 +0100

    fix slot feature for vuetable

--- src/components/Tables/VueTable.vue
+++ src/components/Tables/VueTable.vue
@@ -37,7 +37,7 @@
           </tr>
         </template>
         <tr v-for="(item, a) in values.data" :key="item.id">
-          <td v-if="opts.checkeable">
+          <td v-if="opts.checkeable" width="1%">
             <input
               v-model="item.checked"
               type="checkbox"
@@ -58,7 +58,7 @@
               <slot :name="head.slot" :item="item" :index="a" />
             </template>
 
-            <template v-if="head.editable">
+            <template v-else-if="head.editable">
               <div
                 v-if="head.editable !== 'checkbox'"
                 class="v-table-input-container"
